051923a1cde1fe487fdb6761f149f5b2c40bde20
balto <correo>
2024-10-25 20:30:47 -0300

    primer push

---
+++ src/main/java/com/example/demoXml/DemoXmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoXml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoXmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoXml/DemoXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoXml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
